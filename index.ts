c51d3d60772a53d9b8219feca922084b5d6d0d20
Chandra Suresh <chandra.b.suresh@gmail.com>
2021-02-18 00:20:42 -0800

    Revert resolver changes

--- index.ts
+++ index.ts
@@ -1,12 +1,16 @@
 import Airlock from "airlock-server";
 
+const airtableConfig = {
+  apiKey: process.env.REACT_APP_AIRTABLE_USER_KEY,
+  baseKey: process.env.REACT_APP_AIRTABLE_BASE_KEY,
+};
+
 new Airlock({
-  airtableApiKey: [],
-  airtableBaseId: "",
-  airtableUserTableName: "",
-  airtableUsernameColumn: "",
-  airtablePasswordColumn: "",
-  allowedOrigins: [],
+  airtableApiKey: [airtableConfig.apiKey],
+  airtableBaseId: airtableConfig.baseKey,
+  airtableUserTableName: "Users",
+  airtableUsernameColumn: "username",
+  airtablePasswordColumn: "password",
+  allowedOrigins: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002'],
   port: 3000,
-  resolversDir: './resolvers/index.ts'
 });
--- resolvers/index.ts
+++ resolvers/index.ts
@@ -1,13 +1,1 @@
-import { Tables, Columns } from '../airtable/schema';
-
-module.exports = {
-  [Tables.Farms]: {
-    write: (farms, authedUser) => {
-      return (
-        authedUser &&
-        authedUser.fields['farms'] &&
-        authedUser.fields['Project Group'].includes(farms.id)
-      );
-    }
-  },
-};
+module.exports = {};
