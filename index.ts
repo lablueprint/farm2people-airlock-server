14da362b62e0f870b2293aa0342f5df305dba9ea
Chandra Suresh <chandra.b.suresh@gmail.com>
2021-02-17 23:40:20 -0800

    Revert "Configured Airlock Server"
    This reverts commit 46063593fc122c3550353faafbf4e950568c7d86.

--- index.ts
+++ index.ts
@@ -1,17 +1,12 @@
 import Airlock from "airlock-server";
 
-const airtableConfig = {
-  apiKey: process.env.REACT_APP_AIRTABLE_USER_KEY,
-  baseKey: process.env.REACT_APP_AIRTABLE_BASE_KEY,
-};
-
 new Airlock({
-  airtableApiKey: [airtableConfig.apiKey],
-  airtableBaseId: airtableConfig.baseKey,
-  airtableUserTableName: "Users",
-  airtableUsernameColumn: "username",
-  airtablePasswordColumn: "password",
-  allowedOrigins: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002'],
+  airtableApiKey: [],
+  airtableBaseId: "",
+  airtableUserTableName: "",
+  airtableUsernameColumn: "",
+  airtablePasswordColumn: "",
+  allowedOrigins: [],
   port: 3000,
   resolversDir: './resolvers/index.ts'
 });
