2f622abc8f73bf24e16a116e6bfef970a2560fa7
leo-liu1 <leoliu1@g.ucla.edu>
2021-07-06 20:52:42 -0700

    added code for deploying airlock server and web app on same server

--- server.js
+++
@@ -1,5 +0,0 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 8000;
-app.use(express.static('farm2people-web-app/build'));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- server.ts
+++ server.ts
@@ -1,23 +1,19 @@
 import Airlock from "airlock-server";
-import * as express from "express";
 
 const airtableConfig = {
   apiKey: process.env.REACT_APP_AIRTABLE_USER_KEY,
   baseKey: process.env.REACT_APP_AIRTABLE_BASE_KEY,
 };
 
-const app = express();
 const PORT = 8000;
 
-new Airlock({
-  server: app,
+const airlock = new Airlock({
   airtableApiKey: [airtableConfig.apiKey],
   airtableBaseId: airtableConfig.baseKey,
   airtableUserTableName: "Users",
   airtableUsernameColumn: "username",
   airtablePasswordColumn: "password",
-  allowedOrigins: [`http://localhost:${PORT}`],
+  allowedOrigins: ['http://localhost:8000'],
 });
 
-app.use(express.static('farm2people-web-app/build'));
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
+airlock.server.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
